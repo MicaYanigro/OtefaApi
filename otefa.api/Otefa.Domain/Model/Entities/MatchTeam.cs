85034d8ea36ca2c790f57697a4079e00781e197d
Micaela Yanigro <micayanigro@gmail.com>
2017-09-11 16:22:21 -0300

    virtual properties

--- otefa.api/Otefa.Domain/Model/Entities/MatchTeam.cs
+++ otefa.api/Otefa.Domain/Model/Entities/MatchTeam.cs
@@ -190,7 +190,7 @@ namespace Otefa.Domain.Model.Entities
         }
 
         [Obsolete]
-        public Tournament Tournament { get; set; }
+        public virtual Tournament Tournament { get; set; }
 
         [NotMapped]
         private Tournament tournament
@@ -208,7 +208,7 @@ namespace Otefa.Domain.Model.Entities
         }
 
         [Obsolete]
-        public Match Match { get; set; }
+        public virtual Match Match { get; set; }
 
         [NotMapped]
         private Match match
